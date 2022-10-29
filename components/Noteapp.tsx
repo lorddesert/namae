2de8da77222b7df9736058824092f55667135475
lorddesert <sacha.nocetto@hotmail.com>
2022-10-28 21:37:46 -0300

    add tRPC

--- components/Noteapp.tsx
+++ components/Noteapp.tsx
@@ -1,43 +1,43 @@
+import { useState } from "react"
+import { trpc } from '../utils/trpc';
+import { Note } from "../pages/api/create-notes";
 
 export default function Noteapp() {
+  const allNotes = trpc.getNotes.useQuery()
+  const [notes, setNotes] = useState(allNotes.data?.notes)
 
   async function createNote(e: any) {
     e.preventDefault()
     const [titleInput, bodyInput] = e.target
-    const title = titleInput.value
-    const body = bodyInput.value
+    const title: string = titleInput.value
+    const body: string = bodyInput.value
 
-    const newNoteData = JSON.stringify({
-      title,
-      body
-    })
-
-    const response = await fetch('/api/createNote', {
-      method: "POST",
-      body: newNoteData
-    })
-
-    const data = await response.json()
-
-    console.log(data)
-
-    // await fetch('/api/getNotes')
+    // setNotes(response)
   }
 
-  return <form onSubmit={createNote}>
-    <section>
-      <label htmlFor="note-title">
-        Note title
-        <input type="text" name="note-title" />
-      </label>
-    </section>
-    <section>
-      <label htmlFor="note-body">
-        Content
-        <textarea name="note-body" id="note-body" cols={30} rows={10} />
-      </label>
-    </section>
-    <button type="submit">Create note</button>
-  </form>
+  if (!allNotes.data || !notes) return <h1>Loading...</h1>
+  return <>
+    <form onSubmit={createNote}>
+      <section>
+        <label htmlFor="note-title">
+          Note title
+          <input type="text" name="note-title" />
+        </label>
+      </section>
+      <section>
+        <label htmlFor="note-body">
+          Content
+          <textarea name="note-body" id="note-body" cols={30} rows={10} />
+        </label>
+      </section>
+      <button type="submit">Create note</button>
+    </form>
 
+    <ul>
+      {notes.map((note: Note, i: number) => <li key={`note-${i}`}>
+        <h4>{note.title}</h4>
+        <p>{note.body}</p>
+      </li>)}
+    </ul>
+  </>
 }
