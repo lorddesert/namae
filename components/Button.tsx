0b2ebf5bcd0bea5311273d6238f54fb69433110c
lorddesert <sacha.nocetto@hotmail.com>
2022-10-30 22:55:27 -0300

    add class props

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,12 +1,13 @@
 type ButtonProps = {
   label: string,
-  onClick: any
+  onClick: any,
+  className?: string
 }
 
-export default function Button({ label, onClick }: ButtonProps) {
+export default function Button({className, label, onClick }: ButtonProps) {
   return <button
     onClick={onClick}
-    className=' bg-teal-500 border rounded border-teal-500 py-1 px-2 hover:bg-teal-600 hover:border-teal-600 transition-colors'
+    className={` ${className} bg-teal-500 border rounded border-teal-500 py-1 px-2 hover:bg-teal-600 hover:border-teal-600 transition-colors`}
     type='submit'>
     {label}
   </button>
