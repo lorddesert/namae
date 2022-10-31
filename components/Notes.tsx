9eedf2b165964f0da39adec7c62cd3dad933edaa
lorddesert <sacha.nocetto@hotmail.com>
2022-10-31 15:20:23 -0300

    format and add ID

--- components/Notes.tsx
+++ components/Notes.tsx
@@ -6,13 +6,14 @@ export default function Notes() {
 
   const allNotes = trpc.getNotes.useQuery()
   const notes: TypeNote[] = allNotes.data?.notes
-  
+
   if (!notes) return <h1>Loading...</h1>
 
   return <>
-    <ul>
-      {notes.map(({ title, body }, i: number) => <li key={`note-${i}`}>
-        <Note {...{title, body}} />      </li>)}
+    <ul className="grid grid-cols-3 ">
+      {notes.map(({ id, title, body }) => <li key={`note-${id}`}>
+        <Note {...{ id, title, body }} />
+      </li>)}
     </ul>
   </>
 }
