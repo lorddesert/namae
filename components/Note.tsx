e3b1f32b00231b6f4ded8834a0c921c4153b7f4b
lorddesert <sacha.nocetto@hotmail.com>
2022-10-31 15:20:17 -0300

    add tailwind classes

--- components/Note.tsx
+++ components/Note.tsx
@@ -2,7 +2,7 @@ import { TypeNote } from "../pages/api/create-notes";
 
 export default function Note({ title, body }: TypeNote) {
   return <article className=" text-slate-200 border rounded-md border-slate-700 p-2 m-2">
-    <h4>{title}</h4>
-    <p>{body}</p>
+    <h4 className=" text-2xl mb-4">{title}</h4>
+    <p className=" text-lg">{body}</p>
   </article>
 }
