c27e19cd169ce14de14f59b4078abaa3a99d3573
lorddesert <sacha.nocetto@hotmail.com>
2022-10-30 22:56:02 -0300

    add classes to the Note

--- components/Note.tsx
+++ components/Note.tsx
@@ -1,7 +1,7 @@
 import { TypeNote } from "../pages/api/create-notes";
 
 export default function Note({ title, body }: TypeNote) {
-  return <article className="">
+  return <article className=" text-slate-800 border rounded-md border-slate-700 p-2 m-2">
     <h4>{title}</h4>
     <p>{body}</p>
   </article>
