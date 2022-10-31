640131093914dc30b9d93020adb64921364b4979
lorddesert <sacha.nocetto@hotmail.com>
2022-10-31 15:21:02 -0300

    add subscription event

--- server/routers/_app.ts
+++ server/routers/_app.ts
@@ -1,6 +1,12 @@
-import { readFileSync } from 'fs';
+import { readFileSync, writeFileSync } from 'fs';
 import { z } from 'zod';
+import { TypeNote } from '../../pages/api/create-notes';
 import { publicProcedure, router } from '../trpc';
+import { observable } from '@trpc/server/observable';
+import { EventEmitter } from 'events'
+
+const ee = new EventEmitter()
+
 
 export const appRouter = router({
   getNotes: publicProcedure
@@ -11,6 +17,44 @@ export const appRouter = router({
         notes: data
       };
     }),
+  createNote: publicProcedure
+    .input(
+      z.object({
+        title: z.string(),
+        body: z.string()
+      })
+    )
+    .mutation((req) => {
+      const { input } = req
+      const { title, body } = input
+      const notes = JSON.parse(readFileSync('./notes.json').toString())
+
+      const newNote: TypeNote = {
+        id: globalThis.crypto.randomUUID(),
+        title,
+        body
+      }
+
+      const response = [...notes, newNote]
+
+      writeFileSync('./notes.json', JSON.stringify(response))
+
+      return response
+    }),
+  onAdd: publicProcedure.subscription(() => {
+    return observable((emit) => {
+      const onAdd = (data: TypeNote) => {
+        emit.next(data)
+      }
+
+      ee.on('add', onAdd)
+
+
+      return () => {
+        ee.off('add', onAdd)
+      }
+    })
+  })
 
 });
 
