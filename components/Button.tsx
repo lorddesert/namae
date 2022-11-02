bc4c6c7704462b90523951f95053517b3f094319
lorddesert <sacha.nocetto@hotmail.com>
2022-11-02 00:39:20 -0300

    update component classes

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,14 +1,16 @@
 type ButtonProps = {
   label: string,
-  onClick: any,
-  className?: string
+  onClick?: any,
+  className?: string,
+  type: "submit" | "reset" | "button"
 }
 
-export default function Button({className, label, onClick }: ButtonProps) {
+export default function Button({ type, className, label, onClick }: ButtonProps) {
   return <button
     onClick={onClick}
     className={` ${className} bg-teal-700 border rounded border-teal-700 py-1 px-2 focus:bg-teal-600 focus:border-teal-600 hover:bg-teal-600 hover:border-teal-600 transition-colors`}
-    type='submit'>
+    type={`${type || 'submit'}`}
+  >
     {label}
   </button>
 }
