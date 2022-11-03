481c477b528c791e56f76ccd5d7bf2f9fe0849e4
lorddesert <sacha.nocetto@hotmail.com>
2022-11-03 01:01:00 -0300

    add neko image on not found msg

--- components/Notes.tsx
+++ components/Notes.tsx
@@ -1,14 +1,17 @@
 import { TypeNote } from "../pages/api/create-notes"
 import Note from "./Note"
 import { useGetNotes } from "./hooks/getNotes"
-
+import Image from "next/image"
 
 export default function Notes() {
   const {notes, isLoading} = useGetNotes()
   
   if (isLoading) return <h1>Loading...</h1>
 
-  if (!notes.length) return <h1>No notes found</h1>
+  if (!notes.length) return <div className="flex items-center justify-center w-full gap-4 text-center">
+  <h3>No notes found</h3>
+  <Image src='/neko.svg' alt="not found neko" width={50} height={50}  />
+  </div>
 
   return <>
     <ul className="grid grid-cols-3 ">
