172c3d6df05b8326f971666160f7cbb7758087cf
lorddesert <sacha.nocetto@hotmail.com>
2022-11-03 00:59:34 -0300

    change default value of button type

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,15 +1,17 @@
+import Image from 'next/image'
+
 type ButtonProps = {
   label: string,
   onClick?: any,
   className?: string,
-  type: "submit" | "reset" | "button"
+  type?: "submit" | "reset" | "button"
 }
 
 export default function Button({ type, className, label, onClick }: ButtonProps) {
   return <button
     onClick={onClick}
-    className={` ${className} bg-teal-700 border rounded border-teal-700 py-1 px-2 focus:bg-teal-600 focus:border-teal-600 hover:bg-teal-600 hover:border-teal-600 transition-colors`}
-    type={`${type || 'submit'}`}
+    className={` ${className} bg-teal-700 border rounded border-teal-700 py-1 px-2 focus:bg-teal-600 focus:border-teal-600 hover:bg-teal-600 hover:border-teal-600 transition-colors flex items-center justify-center gap-2`}
+    type={`${type || 'button'}`}
   >
     {label}
   </button>
