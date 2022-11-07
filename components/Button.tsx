dcabe88e129cf72c4b45d8c6784e8937d2a3262a
lorddesert <sacha.nocetto@hotmail.com>
2022-11-06 22:14:37 -0300

    feat: add name and type props to component

--- components/Button.tsx
+++ components/Button.tsx
@@ -2,11 +2,13 @@ type ButtonProps = {
   label: string,
   onClick?: any,
   className?: string,
-  type?: "submit" | "reset" | "button"
+  type?: "submit" | "reset" | "button",
+  name: string
 }
 
-export default function Button({ type, className, label, onClick }: ButtonProps) {
+export default function Button({ type, className, label, onClick, name }: ButtonProps) {
   return <button
+    name={name}
     onClick={onClick}
     className={` ${className} bg-teal-700 border rounded border-teal-700 py-1 px-2 focus:bg-teal-600 focus:border-teal-600 hover:bg-teal-600 hover:border-teal-600 transition-colors flex items-center justify-center gap-2`}
     type={`${type || 'button'}`}
