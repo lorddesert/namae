83c8f88143440293b24c5c6c8d1bb82b68044959
lorddesert <sacha.nocetto@hotmail.com>
2022-11-07 14:03:45 -0300

    feat: finish tokenfield

--- components/Tokenfield.tsx
+++ components/Tokenfield.tsx
@@ -3,23 +3,32 @@ import { useState } from "react"
 import Button from "./Button"
 import Tags from "./Tags"
 
-export default function Tokenfield({ setFormTags }: { setFormTags: any }) {
-  const [tags, setTags] = useState([])
+export default function Tokenfield({ tags, setTags }: { tags: string[], setTags: any }) {
 
   function addTag(e: any) {
     e.preventDefault()
-    const newTag = document.querySelector('input[name="tag-input"]').value
-    const newTags = [...tags, newTag]
-    console.log(newTag)
+    const tagInput = document.querySelector('input[name="tag-input"]')
+    // @ts-ignore
+    const newTag: string = tagInput.value
 
+    if (!newTag.trim()) return
+
+    const newTags = [...tags, newTag]
+
+    // @ts-ignore
     setTags(newTags)
+
+    // @ts-ignore
+    tagInput.value = ""
+    // @ts-ignore
+    tagInput.focus()
   }
   return <section className="py-2" >
     <label htmlFor="note-tags">
       Add tags
       <input type="text" name="tag-input" id="note-tags" />
     </label>
-    <Button label="Add ✖️" onClick={addTag} className="bg-zinc-800 border-zinc-800 hover:bg-zinc-700 hover:border-zinc-700 focus:bg-zinc-700 focus:border-zinc-700" />
+    <Button name='add-tag-btn' label="Add ✖️" onClick={addTag} className="bg-zinc-800 border-zinc-800 hover:bg-zinc-700 hover:border-zinc-700 focus:bg-zinc-700 focus:border-zinc-700" />
 
     <ul className="flex flex-wrap gap-1">
       <Tags tags={tags} />
