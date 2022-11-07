477a0290f321a93a738a92e9540c700d11b5229c
lorddesert <sacha.nocetto@hotmail.com>
2022-11-07 14:03:34 -0300

    feat: add tab index -1

--- components/Tag.tsx
+++ components/Tag.tsx
@@ -1,6 +1,6 @@
 export default function Tag({ text }: { text: string }) {
   return <li>
-    <button className={`px-1 text-indigo-500 rounded-sm text-xs text-bold w-fit bg-slate-900`}>
+    <button tabIndex={-1} className={`px-1 text-indigo-500 rounded-sm text-xs text-bold w-fit bg-slate-900 hover:text-indigo-400 hover:underline`}>
       {text}
     </button>
   </li>
