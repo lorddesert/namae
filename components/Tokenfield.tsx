a3d2c4c97213fa74bc10489ae03c1c19f8c06639
lorddesert <sacha.nocetto@hotmail.com>
2022-11-10 23:47:15 -0300

    feat: finish Tokenfield logic

--- components/Tokenfield.tsx
+++ components/Tokenfield.tsx
@@ -24,11 +24,21 @@ export default function Tokenfield({ tags, setTags }: { tags: string[], setTags:
     tagInput.focus()
   }
   return <section className="py-2" >
-    <label htmlFor="note-tags">
+    <label htmlFor="note-tags" style={{ gap: 0 }}>
+
       Add tags
-      <input type="text" name="tag-input" id="note-tags" />
+      <span className="mt-1 text-xs italic text-slate-400">You can press Enter to create a tag!</span>
+      <div className="flex items-center gap-3 mt-1">
+        <input placeholder="BLAZING!" className="flex-1 placeholder:italic" type="text" name="tag-input" id="note-tags" />
+        <Button
+          name='add-tag-btn'
+          label="Add ✖️"
+          onClick={addTag}
+          className="h-full bg-zinc-800 border-zinc-800 hover:bg-zinc-700 hover:border-zinc-700 focus:bg-zinc-700 focus:border-zinc-700" 
+        />
+
+      </div>
     </label>
-    <Button name='add-tag-btn' label="Add ✖️" onClick={addTag} className="bg-zinc-800 border-zinc-800 hover:bg-zinc-700 hover:border-zinc-700 focus:bg-zinc-700 focus:border-zinc-700" />
 
     <ul className="flex flex-wrap gap-1">
       <Tags tags={tags} />
