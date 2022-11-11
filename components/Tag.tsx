4e5076f8a108e6d1e51e603446af15fa9e4e81e6
lorddesert <sacha.nocetto@hotmail.com>
2022-11-10 23:50:03 -0300

    feat: wrap tag in Link

--- components/Tag.tsx
+++ components/Tag.tsx
@@ -1,8 +1,11 @@
+import Link from "next/link"
 export default function Tag({ text }: { text: string }) {
   return <li>
-    <button tabIndex={-1} className={`px-1 text-indigo-500 rounded-sm text-xs text-bold w-fit bg-slate-900 hover:text-indigo-400 hover:underline`}>
-      {text}
-    </button>
+    <Link href={`/notes-by-tag/${text}`}>
+      <button tabIndex={-1} className={`px-1 text-indigo-500 rounded-sm text-xs text-bold w-fit bg-slate-900 hover:text-indigo-400 hover:underline`}>
+        {text}
+      </button>
+    </Link>
   </li>
 
 }
