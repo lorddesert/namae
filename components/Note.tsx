30c5dd0f87e2d23617b0d23a2c7db0fe5e135caf
lorddesert <sacha.nocetto@hotmail.com>
2022-11-28 00:37:58 -0300

    add link to details

--- components/Note.tsx
+++ components/Note.tsx
@@ -31,27 +31,27 @@ export default function Note({ hideButtons = false, id, title, body, tags, class
 
   }, [noteMutation.isLoading, noteMutation.data, setNotes])
 
-  return <Link href={`/notes/${id}`} className="w-full">
-    <article className={` ${className} p-2 m-2 mx-1 pt-1 min-w-[200px] max-w-[250px] border rounded-md aspect-video text-slate-200 border-slate-900 flex flex-col justify-between gap-3 hover:border-slate-800 hover:bg-slate-900 transition-colors`}>
-      <header className="w-full capitalize">
-        <h4 className="text-lg ">{title}</h4>
-        <Tags tags={tags} />
-      </header>
-      <main>
-        <p className="overflow-hidden text-sm text-ellipsis whitespace-nowrap">{body}</p>
-      </main>
-      {!hideButtons &&
-        <footer className="flex flex-col gap-2">
-          {/* <Button name="go-to-details" label="Details" className="w-full text-sm" /> */}
+  return <article className={` ${className} p-2 m-2 mx-1 pt-1 min-w-[200px] max-w-[250px] border rounded-md aspect-video text-slate-200 border-slate-900 flex flex-col gap-3`}>
+    <header className="w-full capitalize">
+      <h4 className="text-lg ">{title}</h4>
+      <Tags tags={tags} />
+    </header>
+    <main>
+      <p className="overflow-hidden text-sm text-ellipsis whitespace-nowrap">{body}</p>
+    </main>
+    {!hideButtons &&
+      <footer className="flex flex-col gap-2">
+        <Link href={`/notes/${id}`} className="w-full">
+          <Button name="go-to-details" label="Details" className="w-full text-sm" />
+        </Link>
 
-          <Button
-            name="delete-note-button"
-            label="Delete"
-            className="flex-1 p-1 text-sm border-none bg-rose-800 text-slate-200 hover:bg-rose-600 focus:bg-rose-600"
-            onClick={deleteNote}
-          />
-        </footer>
-      }
-    </article>
-  </Link>
+        <Button
+          name="delete-note-button"
+          label="Delete"
+          className="flex-1 p-1 text-sm border-none bg-rose-800 text-slate-200 hover:bg-rose-600 focus:bg-rose-600"
+          onClick={deleteNote}
+        />
+      </footer>
+    }
+  </article>
 }
