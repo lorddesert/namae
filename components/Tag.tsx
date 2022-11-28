7706d0a4b4d980a5b80bda947914d119c51d93e3
lorddesert <sacha.nocetto@hotmail.com>
2022-11-28 00:38:36 -0300

    feat: add tabindex -1

--- components/Tag.tsx
+++ components/Tag.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link"
 export default function Tag({ text }: { text: string }) {
   return <li>
-    <Link href={`/notes-by-tag/${text}`}>
+    <Link href={`/notes-by-tag/${text}`} tabIndex={-1}>
       <button tabIndex={-1} className={`px-1 text-indigo-500 rounded-sm text-xs text-bold w-fit bg-slate-900 hover:text-indigo-400 hover:underline`}>
         {text}
       </button>
